497fa8e093fa061d20bd13fca559436ef7db9aaa
Eric Chen <eric_chen@progressive.com>
2023-09-21 11:10:44 -0400

    Synced with Jeff's updates

--- src/Week 1/BankingSolution/Banking.Domain/Account.cs
+++ src/Week 1/BankingSolution/Banking.Domain/Account.cs
@@ -1,19 +1,17 @@
 ﻿namespace Banking.Domain;
 
-
-
 public class Account
 {
     private decimal _balance = 5000M;
-    public void Deposit(TransactionValueTypes.Deposit amountToDeposit)
+    public virtual void Deposit(TransactionValueTypes.Deposit amountToDeposit)
     {
         _balance += amountToDeposit.Value;
+
     }
-
-
 
     public decimal GetBalance()
     {
+
         return _balance;
     }
     // "Primitive Obsession" -
@@ -23,8 +21,6 @@ public class Account
 
         _balance -= amountToWithdraw.Value; // The important business!
     }
-
-
 
     private void GuardHasSufficientFunds(decimal amountToWithdraw)
     {
